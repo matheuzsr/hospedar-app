1852e00c336132c895645790e94b7c8533143d9f
Matheus Rocha <matheusdasilvarocha@gmail.com>
2025-02-16 11:03:44 -0300

    config: Configurando tailwind e routes, e libs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseKey: process.env.SUPABASE_KEY,
+    jwtSecret: process.env.JWT_SECRET
+  }
 })
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{vue,js}',
+    './components/**/*.{vue,js}',
+    './src/components/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
+    './plugins/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
