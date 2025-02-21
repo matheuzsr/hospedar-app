8b5f8bb20650e7efc96bf61a549e3f89444cc03d
Matheus Rocha <matheusdasilvarocha@gmail.com>
2025-02-20 21:02:45 -0300

    feat: Inclusão dos componentes da shadcn

---
+++ components/ui/button/Button.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from "radix-vue"
+import type { HTMLAttributes } from "vue"
+import type { ButtonVariants } from "."
+import { cn } from "@/lib/utils"
+import { Primitive } from "radix-vue"
+import { buttonVariants } from "."
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants["variant"]
+  size?: ButtonVariants["size"]
+  class?: HTMLAttributes["class"]
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: "button",
+  loading: false,
+})
+
+const sizeIcon = computed(() => {
+  const config: { [key: string]: string } = {
+    sm: "8px",
+    lg: "16px",
+    icon: "10px",
+    default: "12px",
+  }
+  if (!props.size) return config.default
+
+  return config[props.size] || config.default
+})
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="loading"
+  >
+    <template v-if="loading">
+      <span :style="{ '--sizeIcon': sizeIcon }" class="loader" />
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </Primitive>
+</template>
+
+<style lang="scss" scoped>
+.loader {
+  border: 1px solid #f3f3f3;
+  border-top: px solid #f3f3f3;
+  border-radius: 50%;
+  width: var(--sizeIcon);
+  height: var(--sizeIcon);
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
