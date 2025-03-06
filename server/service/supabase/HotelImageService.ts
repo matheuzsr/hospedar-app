1887aced51631b8865e116fb80dd7824a58e3b34
Matheus Rocha <matheusdasilvarocha@gmail.com>
2025-03-05 22:01:34 -0300

    fix: Alterando arquivo de comunicação com o supabase

--- server/service/supabase/HotelImageService.ts
+++ server/service/supabase/HotelImageService.ts
@@ -1,4 +1,4 @@
-import { supabase } from "~/lib/supabase"
+import { supabase } from "~/infra/SupabaseAdapter"
 import { InternalServerError } from "~/server/errors/errors"
 
 export class HotelImageService {
